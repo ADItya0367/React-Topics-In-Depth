c2b20f6767ff37b8c2d57de5a09044fe5552a53d
ADItya0367 <21egjcs001@gitjaipur.com>
2024-04-17 01:37:54 +0530

    concepts covered till 6

---
+++ Concept_06/react_01/src/components/B.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class B extends Component {
+  render({name}) {
+    return (
+      
+        <>
+        <h1>My name is Aditya </h1>
+        <h2>I am Learning React js in Depth {name}</h2>
+
+        </>
+      
+    )
+  }
+}
+
+// here i have created a class component and i have exported it to the A.js file
+// whenever you will change something it will automatically change in the A.js file and in browser too - this concept is 
+// called as HOT RELOADING .............
+
+// now test yourself by creating a new component and exporting it to the A.js file and see the changes in the browser
