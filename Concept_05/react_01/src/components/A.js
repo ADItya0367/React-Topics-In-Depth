8b4e1ef39d875a8d730e1447a5159dbb07168df9
ADItya0367 <21egjcs001@gitjaipur.com>
2024-04-16 23:59:00 +0530

    concepts covered till 5

---
+++ Concept_05/react_01/src/components/A.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import B from './B'
+// this is the import area 
+// here we are wxporting this function to the index.js file
+// and we are importing this function in the index.js file
+
+// if you remember we know that we can export in two ways default and named export 
+// so just create components and export them in the index.js file 
+// and change the path in the index .html file which you want to run  
+export default function A() {
+  return (
+    <div>
+      <h1>Hello brothers!! <B/></h1>
+    </div>
+  )
+}
